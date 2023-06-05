3e43578fb73a13fbb8e3adec7966af2864199637
TudorPCT <tcosmin.pasat@gmail.com>
2023-06-05 15:26:55 +0300

    Update

--- LungDiseaseXRayClassificationBE/src/main/java/com/lungvision/lungdiseasexrayclassificationbe/config/AppConfig.java
+++ LungDiseaseXRayClassificationBE/src/main/java/com/lungvision/lungdiseasexrayclassificationbe/config/AppConfig.java
@@ -1,10 +1,6 @@
 package com.lungvision.lungdiseasexrayclassificationbe.config;
 
-import com.lungvision.lungdiseasexrayclassificationbe.model.Role;
-import com.lungvision.lungdiseasexrayclassificationbe.model.User;
-import com.lungvision.lungdiseasexrayclassificationbe.repository.UserRepository;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
